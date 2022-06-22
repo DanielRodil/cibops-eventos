655630550f3636d757e30b2eafee4c67c4171aad
sdrfdim <sdrfdim@gmail.com>
2022-06-22 14:22:25 +0200

    cargada operacion de inicio en editar evento

--- frontend/src/app/operaciones/eventos/actividadoperativa-editar-form/actividadoperativa-editar-form.component.ts
+++ frontend/src/app/operaciones/eventos/actividadoperativa-editar-form/actividadoperativa-editar-form.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
 import { ActividadoperativaImpl } from '../../models/actividadoperativa-impl';
 import { Operacion } from '../../models/operacion';
 import { EventoService } from '../../service/evento.service';
@@ -14,16 +16,21 @@ export class ActividadoperativaEditarFormComponent implements OnInit {
 
   actividadoperativa: ActividadoperativaImpl = new ActividadoperativaImpl();
   operaciones: Operacion[] = [];
+  
+  private host: string = environment.host;
+  private urlEndPointAO: string = `${this.host}actividadesoperativas`
 
   constructor(private eventoService: EventoService,
               private operacionService: OperacionService, 
               private activatedRoute: ActivatedRoute,
-              private router: Router) { }
+              private http: HttpClient) { }
 
   ngOnInit(): void {
     let id: string = this.cargarActividadOperativa();
     this.eventoService.getActividadOperativa(id).subscribe(res => 
       this.actividadoperativa = this.eventoService.mapearActividadesOperativas(res));
+    setTimeout(() => {
+      this.http.get<any>(`${this.urlEndPointAO}/${id}/operacion`).subscribe(res => this.actividadoperativa.operacion = this.operacionService.mapearOperacion(res).urlOperacion); }, 500);
     this.operacionService.getOperaciones().subscribe((response) => 
       this.operaciones = this.operacionService.extraerOperaciones(response));  
   }
--- frontend/src/app/operaciones/eventos/gestionjudicial-editar-form/gestionjudicial-editar-form.component.ts
+++ frontend/src/app/operaciones/eventos/gestionjudicial-editar-form/gestionjudicial-editar-form.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
 import { GestionjudicialImpl } from '../../models/gestionjudicial-impl';
 import { Operacion } from '../../models/operacion';
 import { EventoService } from '../../service/evento.service';
@@ -15,15 +17,20 @@ export class GestionjudicialEditarFormComponent implements OnInit {
   gestionjudicial: GestionjudicialImpl = new GestionjudicialImpl();
   operaciones: Operacion[] = [];
 
+  private host: string = environment.host;
+  private urlEndPointGJ: string = `${this.host}gestionesjudiciales`
+
   constructor(private eventoService: EventoService,
               private operacionService: OperacionService, 
               private activatedRoute: ActivatedRoute,
-              private router: Router) { }
+              private http: HttpClient) { }
 
   ngOnInit(): void {
     let id: string = this.cargarGestionJudicial();
     this.eventoService.getGestionJudicial(id).subscribe(res => 
       this.gestionjudicial = this.eventoService.mapearGestionesJudiciales(res));
+    setTimeout(() => {
+      this.http.get<any>(`${this.urlEndPointGJ}/${id}/operacion`).subscribe(res => this.gestionjudicial.operacion = this.operacionService.mapearOperacion(res).urlOperacion); }, 500);
     this.operacionService.getOperaciones().subscribe((response) => 
       this.operaciones = this.operacionService.extraerOperaciones(response));  
   }
